0a0d50e30fbef5bcbdc0a04a0525a2f875ccd642
Amanda Sun <amandajaisun@gmail.com>
2024-05-30 17:58:18 -0400

    tests cases adhere to minic

--- final_tests/p3.c
+++ final_tests/p3.c
@@ -2,10 +2,11 @@ extern int read();
 extern void print(int);
 
 int func(int n){
-	int sum = 0;
+	int sum;
 	int i;
 	int a;
 	i = 0;
+	sum = 0;
 	
 	while (i < n){ 
 		a = read();
--- final_tests/p4.c
+++ final_tests/p4.c
@@ -2,10 +2,11 @@ extern int read();
 extern void print(int);
 
 int func(int n){
-	int max = 0;
+	int max;
 	int i;
 	int a;
 	i = 0;
+	max = 0;
 	
 	while (i < n){ 
 		a = read();
